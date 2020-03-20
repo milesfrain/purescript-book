6e57de8bce359a503bef404ea17292a6a8f46af2
Miles Frain <miles.frain@colorado.edu>
2020-03-20 12:59:22 -0600

    Ch10 showNumber fix and more FFI examples

---
+++ exercises/chapter10/test/TestFFI.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var Tg = require('../Test.Gcd');
+var Ts = require('../Test.Shout');
+
+exports.runGcd = function(n, m) {
+  return Tg.gcd(n, m);
+};
+
+exports.runShout = Ts.shout(require('../Data.Show').showNumber)(42);
