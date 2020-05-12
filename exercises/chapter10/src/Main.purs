d8c8b75675f9f9c80031e13ad666958203c4d640
Miles Frain <miles.frain@colorado.edu>
2020-05-12 17:55:30 -0600

    copied original address book

--- exercises/chapter10/src/Main.purs
+++ exercises/chapter10/src/Main.purs
@@ -1,62 +1,148 @@
 module Main where
 
 import Prelude
-
-import Control.Monad.Except (runExcept)
+import Data.AddressBook (PhoneNumber, examplePerson)
+import Data.AddressBook.Validation (Errors, validatePerson')
+import Data.Array (mapWithIndex, updateAt)
 import Data.Either (Either(..))
-import Data.Foldable (foldMap)
-import Data.Generic.Rep (class Generic)
-import Data.Generic.Rep.Show (genericShow)
-import Data.Maybe (Maybe(Nothing))
-import Data.Traversable (traverse)
+import Data.Maybe (Maybe(..), fromMaybe)
+import Data.Tuple (Tuple(..))
 import Effect (Effect)
-import Effect.Alert (alert)
-import Effect.Console (log, logShow)
-import Effect.Storage (getItem, setItem)
-import Foreign (readNullOrUndefined, readString, renderForeignError)
-import Foreign.Class (class Decode, class Encode)
-import Foreign.Generic (decodeJSON, defaultOptions, encodeJSON, genericDecode, genericEncode)
+import Effect.Console (log)
+import Effect.Exception (throw)
+import React.Basic.DOM as D
+import React.Basic.DOM.Events (targetValue)
+import React.Basic.Events (handler)
+import React.Basic.Hooks (ReactComponent, component, element, useState)
+import React.Basic.Hooks as R
+import Web.DOM.NonElementParentNode (getElementById)
+import Web.HTML (window)
+import Web.HTML.HTMLDocument (toNonElementParentNode)
+import Web.HTML.Window (document)
 
-newtype FormData = FormData
-  { firstName  :: String
-  , lastName   :: String
-  }
+-- Note that there's a Purty formmating bug that
+-- adds an unwanted blank line
+-- https://gitlab.com/joneshf/purty/issues/77
+renderValidationErrors :: Errors -> Array R.JSX
+renderValidationErrors [] = []
 
-derive instance genericFormData :: Generic FormData _
-
-instance decodeFormData :: Decode FormData where
-  decode = genericDecode (defaultOptions { unwrapSingleConstructors = true })
-
-instance encodeFormData :: Encode FormData where
-  encode = genericEncode (defaultOptions { unwrapSingleConstructors = true })
-
-instance showFormData :: Show FormData where
-  show = genericShow
-
-loadSavedData :: Effect (Maybe FormData)
-loadSavedData = do
-  item <- getItem "person"
-
+renderValidationErrors xs =
   let
-      savedData = runExcept do
-         json <- traverse readString =<< readNullOrUndefined item
-         traverse decodeJSON json
+    rendererror :: String -> R.JSX
+    rendererror err = D.li_ [ D.text err ]
+  in
+    [ D.div
+        { className: "alert alert-danger row"
+        , children: [ D.ul_ (map rendererror xs) ]
+        }
+    ]
 
-  case savedData of
-       Left err -> do
-          alert $ "Unable to read saved form data: " <> foldMap (("\n" <> _) <<< renderForeignError) err
-          pure Nothing
-       Right mdata -> pure mdata
+-- Helper function to render a single form field with an
+-- event handler to update
+formField :: String -> String -> String -> (String -> Effect Unit) -> R.JSX
+formField name placeholder value setValue =
+  D.label
+    { className: "form-group row"
+    , children:
+        [ D.div
+            { className: "col-sm col-form-label"
+            , children: [ D.text name ]
+            }
+        , D.div
+            { className: "col-sm"
+            , children:
+                [ D.input
+                    { className: "form-control"
+                    , placeholder
+                    , value
+                    , onChange:
+                        let
+                          handleValue :: Maybe String -> Effect Unit
+                          handleValue (Just v) = setValue v
+
+                          handleValue Nothing = pure unit
+                        in
+                          handler targetValue handleValue
+                    }
+                ]
+            }
+        ]
+    }
+
+mkAddressBookApp :: Effect (ReactComponent {})
+mkAddressBookApp =
+  -- incomming \props are unused
+  component "AddressBookApp" \props -> R.do
+    -- `useState` takes a default initial value and returns the
+    -- current value and a way to update the value.
+    -- Consult react-hooks docs for a more detailed explanation of `useState`.
+    Tuple person setPerson <- useState examplePerson
+    let
+      errors = case validatePerson' person of
+        Left e -> e
+        Right _ -> []
+
+      -- helper-function to return array unchanged instead of Nothing if index is out of bounds
+      updateAt' :: forall a. Int -> a -> Array a -> Array a
+      updateAt' i x xs = fromMaybe xs (updateAt i x xs)
+
+      -- helper-function to render a single phone number at a given index
+      renderPhoneNumber :: Int -> PhoneNumber -> R.JSX
+      renderPhoneNumber index phone =
+        formField
+          (show phone."type")
+          "XXX-XXX-XXXX"
+          phone.number
+          (\s -> setPerson _ { phones = updateAt' index phone { number = s } person.phones })
+
+      -- helper-function to render all phone numbers
+      renderPhoneNumbers :: Array R.JSX
+      renderPhoneNumbers = mapWithIndex renderPhoneNumber person.phones
+    pure
+      $ D.div
+          { className: "container"
+          , children:
+              renderValidationErrors errors
+                <> [ D.div
+                      { className: "row"
+                      , children:
+                          [ D.form_
+                              $ [ D.h3_ [ D.text "Basic Information" ]
+                                , formField "First Name" "First Name" person.firstName \s ->
+                                    setPerson _ { firstName = s }
+                                , formField "Last Name" "Last Name" person.lastName \s ->
+                                    setPerson _ { lastName = s }
+                                , D.h3_ [ D.text "Address" ]
+                                , formField "Street" "Street" person.homeAddress.street \s ->
+                                    setPerson _ { homeAddress { street = s } }
+                                , formField "City" "City" person.homeAddress.city \s ->
+                                    setPerson _ { homeAddress { city = s } }
+                                , formField "State" "State" person.homeAddress.state \s ->
+                                    setPerson _ { homeAddress { state = s } }
+                                , D.h3_ [ D.text "Contact Information" ]
+                                ]
+                              <> renderPhoneNumbers
+                          ]
+                      }
+                  ]
+          }
 
 main :: Effect Unit
-main = void do
-  let rec = FormData { firstName: "Bruce"
-                     , lastName: "Wayne"
-                     }
-
-  alert "FFI5"
-  log "Storing data into local storage"
-  setItem "person" $ encodeJSON rec
-  log "Loading data from local storage"
-  loadedRec <- loadSavedData
-  traverse logShow loadedRec
+main = do
+  log "Rendering address book component"
+  -- Get window object
+  w <- window
+  -- Get window's HTML document
+  doc <- document w
+  -- Get "container" element in HTML
+  ctr <- getElementById "container" $ toNonElementParentNode doc
+  case ctr of
+    Nothing -> throw "Container element not found."
+    Just c -> do
+      -- Create AddressBook react component
+      addressBookApp <- mkAddressBookApp
+      let
+        -- Create JSX node from react component. Pass-in empty props
+        app = element addressBookApp {}
+      -- Render AddressBook JSX node in DOM "container" element
+      D.render app c
