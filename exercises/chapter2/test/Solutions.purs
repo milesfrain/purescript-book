99213b00308f23e15c4be49620c61ebe54c1bcba
Miles Frain <miles.frain@colorado.edu>
2020-05-15 23:43:41 -0600

    Ch2 Solutions

--- exercises/chapter2/test/Solutions.purs
+++ exercises/chapter2/test/Solutions.purs
@@ -1,3 +1,11 @@
 module Test.Solutions where
 
 import Prelude
+import Global (readFloat)
+import Math (e, pi, sqrt)
+
+diagonal w h = sqrt (w * w + h * h)
+
+circleArea r = pi * r * r
+
+addE s = readFloat s + e
